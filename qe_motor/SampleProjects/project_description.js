8582a3f3ce860c49173f822a48756d5a875a535d
Tommy Tan <139957627+tommy29tan@users.noreply.github.com>
2024-04-29 10:46:16 +0800

    Update project_description.js

--- qe_motor/SampleProjects/project_description.js
+++ qe_motor/SampleProjects/project_description.js
@@ -11,6 +11,26 @@ function addListItem(id, textContent) {
 
 function updateTextContent(id, textContent) {
   var element = document.getElementById(id);
+  var textWithNewLines = textContent.replace(/\n/g, "<br>");
+
   // Set the text content of the h5 element
-  element.textContent = textContent;
+  element.textContent = textWithNewLines;
 }
+
+function insertNote(textContent) {
+  // Get the element before which you want to insert the new <article> section
+  var referenceElement = document.getElementById('list-features');
+  // Define the HTML content for the new <article> section
+  var newArticleHTML = `
+<article>
+  <header>
+    <h4 class="cls-highlight">Note</h4>
+  </header>
+  <h5 id="txt-note"></h5>
+</article>
+`;
+  // Insert the new <article> section before the reference element
+  referenceElement.insertAdjacentHTML('beforebegin', newArticleHTML);
+  updateTextContent('txt-note', textContent);
+}
+
